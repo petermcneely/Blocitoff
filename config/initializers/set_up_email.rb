8ab56ab8cde57cdc9f711e2a0ad04feacfa73993
Peter McNeely <pete.mcneely@gmail.com>
2016-08-22 22:24:45 -0400

    Add landing page and Devise supported User model to app.

---
+++ config/initializers/set_up_email.rb
@@ -0,0 +1,12 @@
+if Rails.env.development? || Rails.env.production?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address:        'smtp.gmail.com',
+    port:           '587',
+    authentication: :plain,
+    user_name:      ENV['GMAIL_USERNAME'],
+    password:       ENV['GMAIL_PASSWORD'],
+    domain:         'gmail.com',
+    enable_starttls_auto: true
+  }
+end
