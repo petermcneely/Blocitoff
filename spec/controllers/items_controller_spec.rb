1cd86056171a42e720abe6d4bba9ab91b3590c2c
Peter McNeely <pete.mcneely@gmail.com>
2016-08-27 11:07:17 -0400

    Add controller tests for items and users

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
+	login_user
+	describe "GET #create" do
+		let(:my_item) {FactoryGirl.create(:item, user: subject.current_user)}
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
+    	it "returns http success" do
+      		get :create, user_id: subject.current_user.id, item: {name: my_item.name}
+      		expect(response).to redirect_to root_path
+    	end
 
+    	it "creates a new to do item" do
+    		get :create, user_id: subject.current_user.id, item: {name: my_item.name}
+    		expect(Item.last.name).to eq my_item.name
+    	end
+  	end
 end
